202344e43c3f5bc2eaafba905230985a4b0e0890
Chris Wallace <christopher.wallace@students.olin.edu>
2015-12-15 10:56:51 -0500

    Added rough drawing of map

--- plotScript.js
+++ plotScript.js
@@ -2,15 +2,47 @@ window.addEventListener("load",run,false);
 
 var transformScale = d3.scale.linear()
     .domain([0,15])
-    .range([0,800]);
+    .range([0,780]);
 
+var RED_PATH = 1;
+var colorMap = {
+	0: "red",
+	1: "red",
+	2: "blue",
+	3: "orange"
+}
+
+var mapCoordinates = {};
+var network = [];
+
+var dataSetsLoaded = 0;
+var maxDatasets = 2;
 
 function run () {
 	d3.json("data/rawCoordinates.json", function(json) {
     console.log(json);
-    console.log(d3.entries(json));
+    mapCoordinates = json;
+    dataSetsLoaded += 1;
+    if (dataSetsLoaded === maxDatasets) {
+    	drawMap();
+    }
+
+});
+
+	d3.json("data/stationPaths.json", function(json) {
+		console.log(json);
+		network = json;
+		dataSetsLoaded += 1;
+		if (dataSetsLoaded === maxDatasets) {
+			drawMap();
+		}
+	})
+
+}
+
+function drawMap() {
     d3.select("#mbtaMap").selectAll("g")
-    	.data(d3.entries(json))
+    	.data(d3.entries(mapCoordinates))
     	.enter()
     	.append("g")
     		.append("circle")
@@ -23,11 +55,36 @@ function run () {
     		.attr("id", function(d) {
     			return d.key;
     		})
+    		.attr("fill", function(d) {
+    			for (var color = 0; color < network.length; color ++) {
+    				for (var i =0; i < network[color].length; i++) {
+    					if (d.key == network[color][i]) {
+    						return colorMap[color];
+    					}
+    						
+    				}
+    			}
+    		})
     		.attr("r", 5);
-});
 
-	d3.json("data/spiderNetwork.json", function(json) {
-		console.log(json);
-	})
-
+    var mbtaMap = d3.select("#mbtaMap");
+    for (var color = 0; color < network.length; color ++) {
+    	for (var i =0; i < network[color].length - 1; i++) {
+    		console.log("test");
+    		var firstNode = d3.select("#" + network[color][i]);
+    		var secondNode = d3.select("#" + network[color][i+1]);
+    		var firstX  = firstNode.attr("cx");
+			var firstY = firstNode.attr("cy");
+			var secondX = secondNode.attr("cx");
+			var secondY = secondNode.attr("cy");
+			mbtaMap.append("line")
+				.attr("x1", firstX)
+				.attr("x2", secondX)
+				.attr("y1", firstY)
+				.attr("y2", secondY)
+				.attr("stroke-width","2")
+				.attr("stroke", colorMap[color])
+    						
+    	}
+    }
 }
